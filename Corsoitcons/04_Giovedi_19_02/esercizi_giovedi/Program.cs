a2203bc25d7716e727f0e0fba407b0bc6d536cd2
Jomat22 <matteoborello0@gmail.com>
2026-02-19 15:46:56 +0100

    esercizi metodi
    aggiunta esercizi metodi

---
+++ Corsoitcons/04_Giovedi_19_02/esercizi_giovedi/Program.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Globalization;
+using System.Security.Cryptography.X509Certificates;
+
+class newclass
+{
+    public static void Main(String[] args){
+
+    //esercizio metodo 1
+
+/*
+
+    {
+        Console.WriteLine("Inserisci il tuo nome: ");
+
+        string nomeutente = Console.ReadLine();
+
+        StampaSaluto(nomeutente);
+
+    }
+        public static void StampaSaluto(string stamp)
+        {
+            
+            Console.WriteLine($"Il mio nome è : {stamp}");
+        }
+*/
+
+
+    //esercizio metodi 2
+
+/*
+    Console.WriteLine("Inserisci un numero per verificare se è pari o dispari: ");
+
+    string input = Console.ReadLine();
+    int inputnumero = int.Parse(input);
+
+    VerificaPari(inputnumero);
+    }
+
+    public static void VerificaPari(int num)
+    {
+        if (num % 2 == 0)
+        {
+            Console.WriteLine($"Il numero {num} è pari ");
+        }
+        else
+        {
+            Console.WriteLine($"Il numero {num} è dispari");
+        }
+        */
+
+        //esercizio metodi 3
+
+
+/*
+
+        Console.WriteLine("Inserisci numero per fare calcolo");
+        int num = int.Parse(Console.ReadLine());
+
+        Console.WriteLine("Inserisci esponente");
+        int esponente = int.Parse(Console.ReadLine());
+
+        int risulato = CalcolaPotenza(num, esponente);
+
+        Console.WriteLine($"Risulato {num} elevato a {esponente} è : {risulato}");
+    }
+
+        public static int CalcolaPotenza(int num, int esponente){
+
+        int calcolo = (int)Math.Pow(num, esponente);
+
+        return calcolo;
+        }
+        */
+
+
+    }
+}
+    
