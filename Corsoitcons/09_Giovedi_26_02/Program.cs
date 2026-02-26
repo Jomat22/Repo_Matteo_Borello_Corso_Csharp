b6aca759b8f6b1771147ab28209cda3e766fd9b0
Jomat22 <matteoborello0@gmail.com>
2026-02-26 14:27:28 +0100

    esercizi giovedi 28_02

---
+++ Corsoitcons/09_Giovedi_26_02/Program.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+
+class newclass
+{
+    public static void Main(String[] args)
+    {
+        /* esercizio 1 dictionary
+        Dictionary<string, long> rubrica = new Dictionary<string, long>
+        {
+            {"Marco Gallo", 3408562991},
+            {"Andrea Ranieri", 3405401953},
+            {"Giulia Fox", 3400091120},
+        };
+        foreach (var rubric in rubrica)
+        {
+            Console.WriteLine($"Il numero di {rubric.Key} è {rubric.Value}" );
+        }
+        
+*/
+        /*esercizio 2 dictionary
+
+        Console.WriteLine("Inserisci una frase:");
+        string input = Console.ReadLine();
+
+        string[] parole = input.Split(new[] { ' ' }, StringSplitOptions.RemoveEmptyEntries);
+
+        Dictionary<string, int> freq = new Dictionary<string, int>();
+
+        foreach (string parola in parole)
+        {
+            string newpar = parola.ToLower();
+
+            if (freq.ContainsKey(newpar))
+                freq[newpar]++;
+            else
+                freq[newpar] = 1;
+        }
+
+        Console.WriteLine("Frequenza parole:");
+        foreach (var text in freq)
+        {
+            Console.WriteLine($"{text.Key}: {text.Value}");
+        }*/
+
+        /*esercizio 3 dictionary  
+
+        Dictionary<string, long> magazzino = new Dictionary<string, long>();
+        string scelta = "";
+
+        while (scelta != "5")
+        {
+            Console.WriteLine("1.Aggiungi/Modifica  2.Rimuovi  3.Cerca  4.Stampa  5.Esci");
+            scelta = Console.ReadLine();
+
+            if (scelta == "1")
+            {
+                Console.Write("Nome: "); string n = Console.ReadLine();
+                Console.Write("Quantità (+ o -): "); long q = long.Parse(Console.ReadLine());
+                if (magazzino.ContainsKey(n)) magazzino[n] += q; else magazzino[n] = q;
+            }
+            else if (scelta == "2")
+            {
+                Console.Write("Rimuovi: "); magazzino.Remove(Console.ReadLine());
+            }
+            else if (scelta == "3")
+            {
+                Console.Write("Inserisci il nome del prodotto da cercare: ");
+                string n = Console.ReadLine();
+
+                if (magazzino.ContainsKey(n))
+                {
+                    long q = magazzino[n];
+                    Console.WriteLine($"Prodotto presente, Quantità: {q}");
+                }
+                else
+                {
+                    Console.WriteLine("Prodotto non presente");
+                }
+            }
+            else if (scelta == "4")
+            {
+                foreach (var disp in magazzino)
+                    Console.WriteLine($"{disp.Key}: {disp.Value}");
+            }
+        }
+*/
+        
+
+    }
+}
